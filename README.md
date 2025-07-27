4cb0da9fede1872ef4fbd76441300fbef4932ef1
Zhu1276656353 <152978944+Zhu1276656353@users.noreply.github.com>
2025-07-27 16:12:20 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# tunnelling-management-system
-此项目是隧道管理系统，基于vue3框架开发的管理系统
+## 项目使用的技术栈
+1.基础框架：Vue3
+2.网络请求: Axios
+3.状态管理：Pinia
+4.路由：Vue-Router
+5.图标: Echarts
+6.语言切换：Vue-i18n
+7.UI框架：Element Plus
+8.仓库持久化存储:: Pinia-Persistedstate
+
+## 跨域解决方案
+后台cors解决跨域
---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+//按需加载element-plus组件
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+    //按需加载element-plus组件
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+})
